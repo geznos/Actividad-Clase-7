981e1a48cb2b13149b3800c845eca6a8597bb6d0
geznos <garaesp@gmail.com>
2017-03-07 12:45:10 -0400

    Actualizacion

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ function() {
 	console.log("Total compra:");
 	adrian = (kilo_cebolla*3)+(kilo_tomate*2)+(kilo_papa*6)+(kilo_platano/2);
 	leonardo = (kilo_platano*3.5)+(kilo_tomate/2)+(kilo_cambur/4);
-	david = (kilo_recado*3)+(kilo_papa/2)+(kilo_cebolla*3)+(kilo_cambur*3/4);
+	david = (kilo_recado*3)+(kilo_papa*1.5)+(kilo_cebolla*3)+(kilo_cambur*3/4);
 	console.log("Adrian Simancas: "+adrian);
 	console.log("Leonardo Chique: "+leonardo);
 	console.log("David Nalbandian: "+david);
